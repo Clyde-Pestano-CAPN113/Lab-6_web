c60872624128163df88e8a005e15f89f8e330ab6
Clyde-Pestano-CAPN113 <Jalenpestano@icloud.com>
2024-10-08 08:48:16 -0400

    Finished Lab

---
+++ Lab_6.js
@@ -0,0 +1,25 @@
+//Task 1
+let myNumber = 10
+let myString = "Hello, Javascript"
+console.log(myNumber,myString)
+
+//Task 2
+let TestVal = 11
+let TestVal1 = 4
+for (i=0; i < TestVal; i++){
+    console.log(i)
+}
+for (i=0; i < TestVal1; i++){
+    console.log(i * 3)
+}
+
+//Task 3
+function AddNumbers(num1,num2){
+    return(num1 + num2)
+}
+console.log(AddNumbers(3,4))//Output: 7
+
+function MultiplyNumbers(num1,num2){
+    return(num1 * num2)
+}
+console.log(MultiplyNumbers(3,3))//Output: 9
